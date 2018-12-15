17fe320256091476438f6fca7a99b901069c3de7
Laurae <dsoukhavong.cht@gmail.com>
2018-12-15 11:52:09 +0100

    GPU CUDA path for Linux

--- R/xgb.dl.R
+++ R/xgb.dl.R
@@ -1,5 +1,5 @@
 #' Install xgboost from source
-#'
+#' 
 #' Downloads and install xgboost from repository. Allows to customize the commit/branch used. Requires \code{git} and compiler \code{make} (or \code{mingw32-make} for MinGW) in \code{PATH} environment variable. Windows uses \code{\\\\} (backward slashes) while Linux uses \code{/} (forward slashes).
 #' 
 #' @param commit The commit / branch to use. Put \code{""} for master branch. Defaults to \code{"master"}.
@@ -7,6 +7,7 @@
 #' @param repo The link to the repository. Defaults to \code{"https://github.com/dmlc/xgboost"}.
 #' @param use_gpu Whether to install with GPU enabled or not. Defaults to \code{FALSE}. Disabled for Windows + MinGW.
 #' @param use_avx Whether to install with AVX enabled or not. Defaults to \code{FALSE}. Disabled for Windows + MinGW.
+#' @param CUDA Path to CUDA, gcc, and g++ if cmake does not recognize CUDA path. Defaults to \code{list(NULL, NULL, NULL)}. Disabled for Windows. Please specify a list. Example: \code{CUDA = list("/usr/lib/cuda", "/usr/bin/gcc-6", "/usr/bin/g++-6")}.
 #' 
 #' @return A logical describing whether the xgboost package was installed or not (\code{TRUE} if installed, \code{FALSE} if installation failed AND you did not have the package before).
 #' 
@@ -41,9 +42,31 @@
 #' dtest <- xgb.DMatrix(agaricus.test$data, label = agaricus.test$label)
 #' watchlist <- list(train = dtrain, eval = dtest)
 #' 
-#' param <- list(max_depth = 2, eta = 1, silent = 1, nthread = 2, 
+#' param <- list(max_depth = 2, eta = 1, silent = 1, nthread = 2,
 #'               objective = "binary:logistic", eval_metric = "auc")
 #' bst <- xgb.train(param, dtrain, nrounds = 2, watchlist)
+#' 
+#' # Install with GPU support on Linux, CUDA 10 + gcc-6 + g++-6
+#' xgb.dl(compiler = "gcc",
+#'        commit = "a2dc929",
+#'        use_avx = FALSE,
+#'        use_gpu = TRUE,
+#'        CUDA = list("/usr/lib/cuda", "/usr/bin/gcc-6", "/usr/bin/g++-6"))
+#' 
+#' # Test GPU package
+#' library(xgboost)
+#' data(agaricus.train, package = "xgboost")
+#' data(agaricus.test, package = "xgboost")
+#' 
+#' dtrain <- xgb.DMatrix(agaricus.train$data, label = agaricus.train$label)
+#' dtest <- xgb.DMatrix(agaricus.test$data, label = agaricus.test$label)
+#' watchlist <- list(train = dtrain, eval = dtest)
+#' 
+#' param <- list(max_depth = 2, eta = 1, silent = 1, nthread = 2,
+#'               objective = "binary:logistic", eval_metric = "auc",
+#'               max_bin = 64, tree_method = "gpu_hist")
+#' bst <- xgb.train(param, dtrain, nrounds = 2, watchlist)
+#' 
 #' }
 #' 
 #' @export
@@ -52,7 +75,8 @@ xgb.dl <- function(commit = "master",
                    compiler = "gcc",
                    repo = "https://github.com/dmlc/xgboost",
                    use_gpu = FALSE,
-                   use_avx = FALSE) {
+                   use_avx = FALSE,
+                   CUDA = NULL) {
   
   # Generates temporary dir
   xgb_git_dir <- tempdir()
@@ -118,7 +142,11 @@ xgb.dl <- function(commit = "master",
     # Compile
     
     cat(paste0("mkdir build && cd build", "\n"), file = xgb_git_file, append = TRUE)
-    cat(paste0("cmake .. ", ifelse(use_gpu == TRUE, " -DUSE_CUDA=ON", ""), ifelse(use_avx == TRUE, " -DUSE_AVX=ON", ""), " -DR_LIB=ON", "\n"), file = xgb_git_file, append = TRUE)
+    if (is.null(CUDA[[1]]) == TRUE) {
+      cat(paste0("cmake .. ", ifelse(use_gpu == TRUE, " -DUSE_CUDA=ON", ""), ifelse(use_avx == TRUE, " -DUSE_AVX=ON", ""), " -DR_LIB=ON", "\n"), file = xgb_git_file, append = TRUE)
+    } else {
+      cat(paste0("cmake .. ", ifelse(use_gpu == TRUE, paste0(" -DUSE_CUDA=ON -DCUDA_TOOLKIT_ROOT_DIR=", CUDA[[1]], " -DCMAKE_C_COMPILER=", CUDA[[2]], " -DCMAKE_CXX_COMPILER=", CUDA[[3]]), ""), ifelse(use_avx == TRUE, " -DUSE_AVX=ON", ""), " -DR_LIB=ON", "\n"), file = xgb_git_file, append = TRUE)
+    }
     cat(paste0("make install -j", "\n"), file = xgb_git_file, append = TRUE)
     
     # Set permissions on script
